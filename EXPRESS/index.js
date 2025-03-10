5479451471215fdf132a3fa439661dc671bb17ab
xF3rd <fernando.bordin.correa@gmail.com>
2025-03-10 18:31:11 -0300

    Aula de POST

--- EXPRESS/index.js
+++ EXPRESS/index.js
@@ -4,6 +4,29 @@ const port = 3000
 const path = require("path")
 const basePath = path.join(__dirname, 'templates')
 
+app.use(
+    express.urlencoded({
+        extended: true
+    }),
+)
+
+app.use(express.json())
+
+app.get('/users/add',(req, res)=>{
+    res.sendFile(`${basePath}/usersform.html`)
+})
+
+app.post('/users/save',(req, res)=>{
+    console.log(req.body)
+
+    const name = req.body.name
+    const age = req.body.age
+
+    console.log(`O nome do usuário é: ${name} e ele tem ${age} anos`)
+
+    res.sendFile(`${basePath}/usersform.html`)
+})
+
 app.get('/users/:id',(req,res)=>{
     const id = req.params.id
 
