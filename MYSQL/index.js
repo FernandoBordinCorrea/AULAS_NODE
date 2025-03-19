f8452a176e17f74f7846dcabd17ea1f7ac025e05
xF3rd <fernando.bordin.correa@gmail.com>
2025-03-19 13:45:06 -0300

    continuação da aula de SQL+Node

--- MYSQL/index.js
+++ MYSQL/index.js
@@ -6,10 +6,47 @@ const app = express()
 app.engine('handlebars', exphbs.engine())
 app.set('view engine', 'handlebars')
 app.use(express.static('public'))
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+)
+app.use(express.json())
 
 app.get('/', (req, res) => {
 
     res.render('home')
+})
+
+app.post('/books/insertbook', (req, res) => {
+
+    const title = req.body.title
+    const pageqty = req.body.pageqty
+
+    const query = `INSERT INTO books (title, pageqty) VALUES ('${title}','${pageqty}')`
+
+    conn.query(query, function (err) {
+        if (err) {
+            console.log(err)
+        }
+
+        res.redirect('/books')
+    })
+})
+
+app.get('/books', (req, res) => {
+    const query = "SELECT * FROM books"
+
+    conn.query(query, function (err, data) {
+        if (err) {
+            console.log(err)
+        }
+
+        const books = data
+
+        res.render('books', { books })
+
+    })
 })
 
 const conn = mysql.createConnection({
@@ -22,10 +59,10 @@ const conn = mysql.createConnection({
 conn.connect((err) => {
     if (err) {
         console.log(err)
-    } 
+    }
     console.log("Conectado com sucesso!")
     app.listen(3000, () => {
         console.log("Servidor rodando na porta 3000")
-    })  
+    })
 })
 
